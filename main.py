7fbc9586c29b8b981f216633bd49381af4faee50
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-11-26 15:47:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,108 @@
 import json
-brik = 0
-index = 0
-with open("file.json", "r", encoding="utf-8") as a:
-    file = json.load(a)
 
-def all_see():
-    global counter
-    for i in file:
-            print(f"""
-            ID: {i["id"]}, 
-            Имя: {i["name"]},                       
-            Facture: {i["manufacturer"]}, 
-            Benzin: {i["is_petrol"]},    
-            v3: {i["tank_volume"]}
-            """)
+with open("file.json", 'r', encoding='utf-8') as file:
+    i_file = json.load(file)
 
-def kluch_see():
-    global counter
-    found = False
-    id_input = input("Введите ключ: ")
-    for i in file:
-        if id_input == i["id"]:  
-            print(f"""
-            ID: {i["id"]}, 
-            Имя: {i["name"]},                       
-            Facture: {i["manufacturer"]}, 
-            Benzin: {i["is_petrol"]},    
-            v3: {i["tank_volume"]}
-            """)
-            found = True
-            break
-        if not found:
-            print("Такой записи нет :(")
-def add_see():
-    global counter
-    add_input = input("Введите ключ для добавления: ")
-    found_2 = False
-    for i in file:
-        if i["id"] == add_input:
-            found_2 = True
-            print("Такой ключ уже есть")
-            break
-        else:
-            add_name_input = input("Введите название машины: ")
-            add_creator_input = input("Введите название производителя: ")
-            add_bool_input = input("Заправляется ли машина бензином (да/нет): ")
-            add_cub_input = input("Введите объём бака в литрах: ")
 
-            repository = {
-                "id": add_input,
-                "name": add_name_input,
-                "manufacturer": add_creator_input,
-                "is_petrol": True if add_bool_input == "да" else False,
-                "tank_volume": add_cub_input,   
-            }
-            file.append(repository)
-            with open("file.json", "w", encoding="utf-8") as b:
-                json.dump(file, b)  
-
-def del_kluch_see():
-    global counter
-    del_input = int(input("Введите ключ для удаления: "))
-    found_3 = False
-    try:
-        for i in file:
-            if del_input == i["id"]:
-                file.remove(i)
-        with open("file.json", "w", encoding="utf-8") as b:
-            json.dump(file, b)
-    except StopIteration:
-        print("Такой записи нет")
-
-def end_see():
-    global brik
-    print("Вы закрыли программу, ПОЗДРАВЛЯЮ ХЫЦКЖСФЮЪКСЪ")
-    brik += 1
-
-def not_end():
-    print("ТЫ ЧОТ ПОПУТАЛ ПОХОДУ")
+close = 0
+dizel = True
 
 def menu():
-    print("""==================================
-1 - Вывести все записи
-2 - Вывести запись по ключу
-3 - Добавить запись
-4 - Удалить запись по ключу
-5 - Выйти из программы
-==================================""")
+    print("""
+        1 - Вывести все записи 
+        2 - Вывести запись по полю 
+        3 - Добавить запись 
+        4 - Удалить запись по полю 
+        5 - Выйти из программы
+        """)
+
+def all_see():
+    for i in i_file:
+        print(f"""
+        Код: {i['id']}, 
+        Имя: {i['name']},                       
+        Производитель: {i['manufacturer']}, 
+        Бензин: {i['is_petrol']},    
+        Объем бакa: {i['tank_volume']} 
+        """)
+
+def kluch_see():
+    idnum = int(input("Введите номер машины: "))
+    for i in i_file:
+        if idnum == i['id']:
+            print(f"""
+            Код: {i['id']}, 
+            Имя: {i['name']},                       
+            Производитель: {i['manufacturer']}, 
+            Бензин: {i['is_petrol']},    
+            Объем бака: {i['tank_volume']}
+            """)
+            return
+    print("Машина не найдена")
+
+def add_see():
+    brik = False
+    ids = int(input("Введите номер машины: "))
+    
+    for i in i_file:
+        if i['id'] == ids:
+            brik = True
+        break
+    
+    if brik:
+        print("Машина с таким номером уже существует.")
+    else:
+        name = input("Введите имя машины: ")  
+        manufacturer = input("Введите завод изготовитель: ")  
+        is_petrol = input("бензин ? введите да/нет: ")  
+        tank_volume = float(input("Введите объем бака машины: "))  
+
+        new_i = {
+            'id': ids,
+            'name': name,
+            'manufacturer': manufacturer,
+            'is_petrol': True if is_petrol == 'да' else dizel == False, 
+            'tank_volume': tank_volume
+        }
+
+        i_file.append(new_i) 
+        with open("file.json", 'w', encoding='utf-8') as output_file: 
+            json.dump(i_file, output_file, ensure_ascii=False, indent=2)
+        print("Машина успешно добавлена.")
+           
+
+def del_kluch_see():
+    iddel = int(input("Введите номер машины: "))
+    for i in i_file:
+        if iddel == i['id']:
+            i_file.remove(i)
+            with open("file.json", 'w', encoding='utf-8') as output_file:
+                json.dump(i_file, output_file, ensure_ascii=False, indent=2)
+            print("Машина успешно удалена.")
+            return
+    print("Запись не найдена.")
+
+def end_see():
+    global close
+    print(f"!!!!!!!!!!!! Программа завершена !!!!!!!!!! ")
+    close += 1 
 
 def main():
-    while brik != 1:
+    while close != 1:
         menu()
-        user_input = int(input("Введите действие: "))
-        if user_input == 1:
+        point = int(input("Введите номер действия, которое хотите выполнить: "))
+
+        if point == 1:
             all_see()
-        elif user_input == 2:
+        elif point == 2:
             kluch_see()
-        elif user_input == 3:
+        elif point == 3:
             add_see()
-        elif user_input == 4:
+        elif point == 4:
             del_kluch_see()
-        elif user_input == 5:
+        elif point == 5:
             end_see()
         else:
-            not_end()
+            print("Некорректный ввод. Пожалуйста, выберите номер от 1 до 5.")
 
 main()
